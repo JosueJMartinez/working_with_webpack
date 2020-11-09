3e12914cf5e36e98be465870becf544349411d97
JosueJMartinez <jmarti61@gmail.com>
2020-11-09 13:10:31 -0600

    added webpack.config.js reverted customization settings back to normal

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "test.js",
-    path: path.resolve(__dirname, "new"),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
