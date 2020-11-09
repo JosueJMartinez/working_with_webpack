036e1189b7858700e385db4fdb8af61aecf7d40b
JosueJMartinez <jmarti61@gmail.com>
2020-11-09 13:08:48 -0600

    added webpack.config.js for more possible customization

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "test.js",
+    path: path.resolve(__dirname, "new"),
+  },
+};
