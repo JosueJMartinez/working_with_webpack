af349ecba68c8d1db894d229c594f8d31c74ce91
root <jmarti61@gmail.com>
2020-11-10 04:23:47 +0000

    added cache busting and template html functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new HtmlWebpackPlugin({template:'./src/template.html'})],	
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+		test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader" //1. Turns sass into css
+        ]
       },
     ],
   },
